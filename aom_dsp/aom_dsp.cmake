wjdz99/aom

Add NEON version of aom_sum_squares_2d_i16
speedup
4x4	4.7
4x8	4.8
8x4	3.7
8x8	4.7
8x16	4.6
16x8	4.4
16x16	4.5
16x32	4.5
32x16	4.6
32x32	4.5
32x64	4.6
64x32	4.6
64x64	4.5
64x128	4.7
128x64	4.8
128x128	4.7
4x16	7.3
16x4	3.8
8x32	4.7
32x8	4.6
16x64	4.5
64x16	4.7
via SumSquaresTest*DISABLED_Speed

Change-Id: Ib4c891278a732d5d1c779ba86d70e226ed8f45ba

aom_dsp/aom_dsp.cmake
@@ -126,7 +126,8 @@ list(APPEND AOM_DSP_COMMON_INTRIN_NEON "${AOM_ROOT}/aom_dsp/arm/fwd_txfm_neon.c"
             "${AOM_ROOT}/aom_dsp/arm/loopfilter_neon.c"
             "${AOM_ROOT}/aom_dsp/arm/intrapred_neon.c"
             "${AOM_ROOT}/aom_dsp/arm/subtract_neon.c"
-            "${AOM_ROOT}/aom_dsp/arm/blend_a64_mask_neon.c")
+            "${AOM_ROOT}/aom_dsp/arm/blend_a64_mask_neon.c"
+            "${AOM_ROOT}/aom_dsp/arm/sum_squares_neon.c")
 
 list(APPEND AOM_DSP_COMMON_INTRIN_DSPR2
             "${AOM_ROOT}/aom_dsp/mips/common_dspr2.c"

BlueSwordM/aom-av1-psy

aom_dsp/aom_dsp.cmake
@@ -281,7 +281,8 @@ if(CONFIG_AV1_ENCODER)
               "${AOM_ROOT}/aom_dsp/arm/variance_neon.c"
               "${AOM_ROOT}/aom_dsp/arm/hadamard_neon.c"
               "${AOM_ROOT}/aom_dsp/arm/avg_neon.c"
-              "${AOM_ROOT}/aom_dsp/arm/sse_neon.c")
+              "${AOM_ROOT}/aom_dsp/arm/sse_neon.c"
+              "${AOM_ROOT}/aom_dsp/arm/sum_squares_neon.c")
 
   list(APPEND AOM_DSP_ENCODER_INTRIN_MSA "${AOM_ROOT}/aom_dsp/mips/sad_msa.c"
               "${AOM_ROOT}/aom_dsp/mips/subtract_msa.c"