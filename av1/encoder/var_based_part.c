098327e0a20fbbcf8e942261f43b30be8475f903
Wan-Teh Chang <wtc@google.com>
2020-06-11 12:32:44 -0700

    chroma_check: don't access UV planes if monochrome
    In chroma_check(), return immediately if we are generating a monochrome
    encoded stream. This avoids accessing the U, V planes when they are
    NULL.
    
    BUG=aomedia:2723
    
    Change-Id: I26a39791f820b4d4e1d63ff7141f594c3c7181f5

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -583,7 +583,7 @@ static AOM_INLINE void chroma_check(AV1_COMP *cpi, MACROBLOCK *x,
   int i;
   MACROBLOCKD *xd = &x->e_mbd;
 
-  if (is_key_frame) return;
+  if (is_key_frame || cpi->oxcf.monochrome) return;
 
   for (i = 1; i <= 2; ++i) {
     unsigned int uv_sad = UINT_MAX;
