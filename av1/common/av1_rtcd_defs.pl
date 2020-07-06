0a7c8715d79222adf6cfc4c1cdc3fcb8fb3951f3
Jerome Jiang <jianj@google.com>
2020-07-06 20:11:11 +0000

    Revert "Add NEON version of av1_get_nz_map_contexts function"
    This reverts commit 7756703c5b8f9256f4dbdd9ae00e796b4f7cfe99.
    
    Reason for revert: Causing SIGBUS in WebRTC AV1 tests.
    
    BUG=webrtc:11751
    
    Change-Id: I166e92823162603e059b4c3e8749423f635049b9

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -375,7 +375,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
 
   # txb
   add_proto qw/void av1_get_nz_map_contexts/, "const uint8_t *const levels, const int16_t *const scan, const uint16_t eob, const TX_SIZE tx_size, const TX_CLASS tx_class, int8_t *const coeff_contexts";
-  specialize qw/av1_get_nz_map_contexts sse2 neon/;
+  specialize qw/av1_get_nz_map_contexts sse2/;
   add_proto qw/void av1_txb_init_levels/, "const tran_low_t *const coeff, const int width, const int height, uint8_t *const levels";
   specialize qw/av1_txb_init_levels sse4_1 avx2 neon/;
 
