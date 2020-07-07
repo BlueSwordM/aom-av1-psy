1fb017240797bafc86c20d9b832a793a71a87c89
Paul Wilkins <paulwilkins@google.com>
2020-07-07 17:45:46 +0100

    Added documentation of GF and KF groups
    Change-Id: I79c9de9edd8b4a05f4fc4dd91819aa238f3b6fa6

--- av1/encoder/ratectrl.h
+++ av1/encoder/ratectrl.h
@@ -153,21 +153,33 @@ typedef struct {
    */
   int frames_till_gf_update_due;
 
-  /*!\cond */
-
-  // number of determined gf group length left
+  /*!
+   * Number of determined gf groups left
+   */
   int intervals_till_gf_calculate_due;
-  // stores gf group length intervals
+
+  /*!
+   * Stores the determined gf group lengths for a set of gf groups
+   */
   int gf_intervals[MAX_NUM_GF_INTERVALS];
-  // the current index in gf_intervals
+
+  /*!
+   * The current group's index into gf_intervals[]
+   */
   int cur_gf_index;
 
+  /*!\cond */
   int min_gf_interval;
   int max_gf_interval;
   int static_scene_max_gf_interval;
   int baseline_gf_interval;
   int constrained_gf_group;
+  /*!\endcond */
+  /*!
+   * Frames before the next key frame
+   */
   int frames_to_key;
+  /*!\cond */
   int frames_since_key;
   int this_key_frame_forced;
   int next_key_frame_forced;
