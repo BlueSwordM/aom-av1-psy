wjdz99/aom

Add unit test to check if tile config is respected
UniformTileConfigTestLarge checks whether number of tile
rows and tile columns are respected as configured by the user.

This test currently only supports uniform spacing.

Change-Id: I46698b090308c8e88f118e35d677dc0a11865fa4

test/test.cmake
@@ -128,6 +128,7 @@ if(NOT BUILD_SHARED_LIBS)
                 "${AOM_ROOT}/test/film_grain_table_test.cc"
                 "${AOM_ROOT}/test/fwd_kf_test.cc"
                 "${AOM_ROOT}/test/kf_test.cc"
+                "${AOM_ROOT}/test/tile_config_tests.cc"
                 "${AOM_ROOT}/test/sb_multipass_test.cc"
                 "${AOM_ROOT}/test/segment_binarization_sync.cc"
                 "${AOM_ROOT}/test/superframe_test.cc"

BlueSwordM/aom-av1-psy

test/test.cmake
@@ -131,6 +131,7 @@ if(NOT BUILD_SHARED_LIBS)
                 "${AOM_ROOT}/test/sb_multipass_test.cc"
                 "${AOM_ROOT}/test/segment_binarization_sync.cc"
                 "${AOM_ROOT}/test/superframe_test.cc"
+                "${AOM_ROOT}/test/tile_config_test.cc"
                 "${AOM_ROOT}/test/tile_independence_test.cc"
                 "${AOM_ROOT}/test/temporal_filter_test.cc")
     if(CONFIG_REALTIME_ONLY)