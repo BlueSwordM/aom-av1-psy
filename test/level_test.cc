a9c86efc0a07be8a24056c7747258f35f06ff29c
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2020-07-14 13:47:32 +0530

    Fix total_time_encoded calc in AV1LevelStats
    Level monitoring stats are reset every key frame,
    hence total_compressed_size would be reset to 0, but
    total_time_encoded would still be maintained as time
    encoded from the first frame. This is incorrect and
    caused the average bitrate to drop significantly in
    the monitoring process after a keyframe. This commit
    resets the total_time_encoded after every key frame, to
    calculate the correct average bit-rate, correctly over
    each key interval.
    
    BUG=aomedia:2739
    
    Change-Id: I9d6394ca4e520a4fac9cf743c6ff4bf0a6cbf27d

--- test/level_test.cc
+++ test/level_test.cc
@@ -129,13 +129,14 @@ TEST_P(LevelTest, TestLevelMonitoringLowBitrate) {
 
 TEST_P(LevelTest, TestLevelMonitoringHighBitrate) {
   // To save run time, we only test speed 4.
-  if (cpu_used_ == 4 && encoding_mode_ == ::libaom_test::kTwoPassGood) {
+  if (cpu_used_ == 4) {
     libaom_test::I420VideoSource video("hantro_collage_w352h288.yuv", 352, 288,
                                        30, 1, 0, 40);
     target_level_ = kLevelKeepStats;
     cfg_.rc_target_bitrate = 4000;
+    cfg_.g_limit = 40;
     ASSERT_NO_FATAL_FAILURE(RunLoop(&video));
-    ASSERT_EQ(level_[0], 0);
+    ASSERT_EQ(level_[0], 4);
   }
 }
 
