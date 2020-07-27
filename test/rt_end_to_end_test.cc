41ed65041d09fd7c74c94cdc5fabb23609fb631a
Marco Paniconi <marpan@google.com>
2020-07-27 09:28:54 -0700

    rtc: Add speed 9 to rt_end_to_end test
    Add speed 9 and adjust some psnr thresholds.
    
    Change-Id: I2af4c13c37ca600956aebfec9e493f0dd2b16fab

--- test/rt_end_to_end_test.cc
+++ test/rt_end_to_end_test.cc
@@ -35,17 +35,20 @@ std::unordered_map<std::string,
                          { { 5, { { 0, 35.4 }, { 3, 36.3 } } },
                            { 6, { { 0, 35.3 }, { 3, 36.2 } } },
                            { 7, { { 0, 34.9 }, { 3, 35.8 } } },
-                           { 8, { { 0, 35.0 }, { 3, 35.8 } } } } },
+                           { 8, { { 0, 35.0 }, { 3, 35.8 } } },
+                           { 9, { { 0, 34.9 }, { 3, 35.7 } } } } },
                        { "paris_352_288_30.y4m",
                          { { 5, { { 0, 36.2 }, { 3, 36.7 } } },
                            { 6, { { 0, 36.1 }, { 3, 36.5 } } },
                            { 7, { { 0, 35.5 }, { 3, 36.0 } } },
-                           { 8, { { 0, 36.0 }, { 3, 36.5 } } } } },
+                           { 8, { { 0, 36.0 }, { 3, 36.5 } } },
+                           { 9, { { 0, 35.7 }, { 3, 36.3 } } } } },
                        { "niklas_1280_720_30.y4m",
                          { { 5, { { 0, 34.4 }, { 3, 34.4 } } },
                            { 6, { { 0, 34.2 }, { 3, 34.2 } } },
-                           { 7, { { 0, 33.7 }, { 3, 33.6 } } },
-                           { 8, { { 0, 33.6 }, { 3, 33.4 } } } } } };
+                           { 7, { { 0, 33.6 }, { 3, 33.6 } } },
+                           { 8, { { 0, 33.5 }, { 3, 33.5 } } },
+                           { 9, { { 0, 33.4 }, { 3, 33.4 } } } } } };
 
 typedef struct {
   const char *filename;
@@ -162,13 +165,13 @@ TEST_P(RTEndToEndTest, EndtoEndPSNRTest) { DoTest(); }
 TEST_P(RTEndToEndTestThreaded, EndtoEndPSNRTest) { DoTest(); }
 
 AV1_INSTANTIATE_TEST_SUITE(RTEndToEndTest, ::testing::ValuesIn(kTestVectors),
-                           ::testing::Range(5, 9),
+                           ::testing::Range(5, 10),
                            ::testing::Values<unsigned int>(0, 3),
                            ::testing::Values(1), ::testing::Values(1));
 
 AV1_INSTANTIATE_TEST_SUITE(RTEndToEndTestThreaded,
                            ::testing::ValuesIn(kTestVectors),
-                           ::testing::Range(5, 9),
+                           ::testing::Range(5, 10),
                            ::testing::Values<unsigned int>(0, 3),
                            ::testing::Range(2, 5), ::testing::Range(2, 5));
 }  // namespace
