wjdz99/aom

Add ref buffer management state enum
Change-Id: I809a5177ec0fb0b79f61b65390b6772c926c2743

av1/encoder/ratectrl.h
@@ -76,6 +76,8 @@ enum {
   FRAME_UPDATE_TYPES
 } UENUM1BYTE(FRAME_UPDATE_TYPE);
 
+enum { RESET, UPDATE, REFBUF_STATES } UENUM1BYTE(REFBUF_STATE);
+
 typedef enum {
   NO_RESIZE = 0,
   DOWN_THREEFOUR = 1,  // From orig to 3/4.

BlueSwordM/aom-av1-psy

av1/encoder/ratectrl.h
@@ -76,6 +76,8 @@ enum {
   FRAME_UPDATE_TYPES
 } UENUM1BYTE(FRAME_UPDATE_TYPE);
 
+enum { REFBUF_RESET, REFBUF_UPDATE, REFBUF_STATES } UENUM1BYTE(REFBUF_STATE);
+
 typedef enum {
   NO_RESIZE = 0,
   DOWN_THREEFOUR = 1,  // From orig to 3/4.