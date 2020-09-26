9d12ff8c5fc2ae5177d043545449fd5ea8222c06
Wan-Teh Chang <wtc@google.com>
2020-09-25 17:07:13 -0700

    Simplify the condition for av1_hash_table_destroy
    The boolean variable hash_table_created was added in
    https://aomedia-review.googlesource.com/c/aom/+/108881. It is exactly
    the condition under which we need to call av1_hash_table_destroy, so the
    conditional expression can be simplified to hash_table_created.
    
    Change-Id: I83a0346463a59f3d7e59961247a0b09efc648575

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -1467,9 +1467,7 @@ static AOM_INLINE void encode_frame_internal(AV1_COMP *cpi) {
     }
   }
 
-  if ((!is_stat_generation_stage(cpi) && av1_use_hash_me(cpi) &&
-       !cpi->sf.rt_sf.use_nonrd_pick_mode) ||
-      hash_table_created) {
+  if (hash_table_created) {
     av1_hash_table_destroy(&intrabc_hash_info->intrabc_hash_table);
   }
 }
