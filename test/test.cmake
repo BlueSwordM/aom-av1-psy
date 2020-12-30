c10bb981e2c3c3cc34ea4a742af07377ed36508c
Bohan Li <bohanli@google.com>
2020-12-29 16:13:16 -0800

    Add unit tests for the key & value API
    BUG=aomedia:2875
    
    Change-Id: I6e01ee8fd091348d78229858c0dcc87d0d719cb2

--- test/test.cmake
+++ test/test.cmake
@@ -94,6 +94,7 @@ list(APPEND AOM_TEST_INTRA_PRED_SPEED_SOURCES "${AOM_GEN_SRC_DIR}/usage_exit.c"
 if(NOT BUILD_SHARED_LIBS)
   list(APPEND AOM_UNIT_TEST_COMMON_SOURCES
               "${AOM_ROOT}/test/av1_common_int_test.cc"
+              "${AOM_ROOT}/test/av1_key_value_api_test.cc"
               "${AOM_ROOT}/test/cdef_test.cc"
               "${AOM_ROOT}/test/cfl_test.cc"
               "${AOM_ROOT}/test/convolve_test.cc"
