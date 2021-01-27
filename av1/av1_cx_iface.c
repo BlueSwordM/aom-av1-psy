0690e68dc423e677b2b96676f580e7358788124b
Wan-Teh Chang <wtc@google.com>
2021-01-27 14:07:13 -0800

    Add LLVM bug no. for clang-cl static_assert issue
    BUG=aomedia:2875
    
    Change-Id: Idb321de5511991ba3394db0cbeb6c723fc7d71ab

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -2791,7 +2791,8 @@ static aom_codec_err_t encoder_set_option(aom_codec_alg_priv_t *ctx,
 
 #if __STDC_VERSION__ >= 201112L
   // We use the keyword _Static_assert because clang-cl does not allow the
-  // convenience macro static_assert to be used in function scope.
+  // convenience macro static_assert to be used in function scope. See
+  // https://bugs.llvm.org/show_bug.cgi?id=48904.
   _Static_assert(sizeof(ctx->cpi->common.error.detail) >= ARG_ERR_MSG_MAX_LEN,
                  "The size of the err_msg buffer for arg_match_helper must be "
                  "at least ARG_ERR_MSG_MAX_LEN");
