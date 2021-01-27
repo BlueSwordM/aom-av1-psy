14be3cd9f1fa6a661a0f9ab02c6c727419065855
Vishesh <vishesh.garg@ittiam.com>
2021-01-27 15:21:15 +0530

    Segregate utility functions from txb_rdopt.c
    This CL segregates utility functions for RD operations from
    txb_rdopt.c to a new header file txb_rdopt_utils.h.
    
    Change-Id: I0baf695c200018c60d356dc132a3b939c7dbc68b

--- av1/av1.cmake
+++ av1/av1.cmake
@@ -246,6 +246,7 @@ list(APPEND AOM_AV1_ENCODER_SOURCES
             "${AOM_ROOT}/av1/encoder/tx_search.h"
             "${AOM_ROOT}/av1/encoder/txb_rdopt.c"
             "${AOM_ROOT}/av1/encoder/txb_rdopt.h"
+            "${AOM_ROOT}/av1/encoder/txb_rdopt_utils.h"
             "${AOM_ROOT}/av1/encoder/intra_mode_search.c"
             "${AOM_ROOT}/av1/encoder/intra_mode_search.h"
             "${AOM_ROOT}/av1/encoder/intra_mode_search_utils.h"
