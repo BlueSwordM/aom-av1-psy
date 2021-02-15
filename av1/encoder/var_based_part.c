ff5d8dd973dde866fa93bb200547ac69575832b4
Deepa K G <deepa.kg@ittiam.com>
2021-02-15 14:37:44 +0530

    Add call to aom_clear_system_state()
    Added aom_clear_system_state() in the function
    setup_planes() before floating point instructions.
    
    BUG=aomedia:2956
    
    Change-Id: I21d07e64fcb766d1f3e9bacf0093dc2c849d305c

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -812,6 +812,7 @@ static void setup_planes(AV1_COMP *cpi, MACROBLOCK *x, unsigned int *y_sad,
 
   // Pick the ref frame for partitioning, use golden frame only if its
   // lower sad.
+  aom_clear_system_state();
   if (*y_sad_g < 0.9 * *y_sad) {
     av1_setup_pre_planes(xd, 0, yv12_g, mi_row, mi_col,
                          get_ref_scale_factors(cm, GOLDEN_FRAME), num_planes);
