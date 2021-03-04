bee7eb1745aa7cef56863d64f2bc0682ed99d79f
Yunqing Wang <yunqingwang@google.com>
2021-03-04 12:21:07 -0800

    Lower square partition only threshold for 4K encoding
    This change was for speed 6 4k+ encoding, which gave 4% encoder
    speedup with 0.34% BDrate increase.
    
    STATS_CHANGED
    
    Change-Id: I42d8ef9debab72f08845617fcfcaac2626d5a4e1

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -613,10 +613,7 @@ static void set_good_speed_feature_framesize_dependent(
       sf->inter_sf.mv_cost_upd_level = 2;
     }
 
-    // TODO(yunqing): use BLOCK_32X32 for >= 4k.
-    if (is_4k_or_larger) {
-      sf->part_sf.use_square_partition_only_threshold = BLOCK_64X64;
-    } else if (is_720p_or_larger) {
+    if (is_720p_or_larger) {
       sf->part_sf.use_square_partition_only_threshold = BLOCK_32X32;
     } else {
       sf->part_sf.use_square_partition_only_threshold = BLOCK_16X16;
