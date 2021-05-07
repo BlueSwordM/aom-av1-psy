f2f93050113dc9434c84b9d7b71cbef5718fd7d8
Jayasanker J <jayasanker.j@ittiam.com>
2021-05-07 18:11:41 +0530

    Fix android build failure in sharpness_test.cc
    The unordered_map declared in test/sharpness_test.cc uses an enum
    as key. This CL replaces the enum with integer type to fix the
    build error observed with C++11.
    
    BUG=aomedia:3049
    
    Change-Id: I2e60654b3c5993a437db4e018875aab0f88801d4

--- test/sharpness_test.cc
+++ test/sharpness_test.cc
@@ -26,13 +26,12 @@ const unsigned int kCqLevel = 18;
 // List of psnr thresholds for different test combinations
 // keys: test-mode, cpu-used, sharpness.
 const std::unordered_map<
-    ::libaom_test::TestMode,
-    std::unordered_map<int, std::unordered_map<int, double>>>
-    kPsnrThreshold = { { ::libaom_test::kTwoPassGood,
+    int, std::unordered_map<int, std::unordered_map<int, double>>>
+    kPsnrThreshold = { { static_cast<int>(::libaom_test::kTwoPassGood),
                          { { 2, { { 2, 37.6 }, { 5, 37.6 } } },
                            { 4, { { 2, 37.5 }, { 5, 37.5 } } },
                            { 6, { { 2, 37.5 }, { 5, 37.5 } } } } },
-                       { ::libaom_test::kAllIntra,
+                       { static_cast<int>(::libaom_test::kAllIntra),
                          { { 3, { { 2, 42.3 }, { 5, 42.4 } } },
                            { 6, { { 2, 41.8 }, { 4, 41.9 }, { 5, 41.9 } } },
                            { 9, { { 2, 41.4 }, { 5, 41.4 } } } } } };
