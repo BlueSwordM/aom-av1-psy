29a801a6fe089e482d9f066ce467b577ef37e2ad
Fyodor Kyslov <kyslov@google.com>
2021-06-01 20:13:51 -0700

    AllIntraMode: Clean up unused speed settings
    Currently the encoder produces same results on speed 7, 8 and 9.
    Cleaning speed features settings for these speeds for future development
    
    BUG=aomedia:3022
    
    Change-Id: I51570539ccd23c4c28378dc121a6035200e862ee

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -153,7 +153,6 @@ static int frame_is_boosted(const AV1_COMP *cpi) {
 static void set_allintra_speed_feature_framesize_dependent(
     const AV1_COMP *const cpi, SPEED_FEATURES *const sf, int speed) {
   const AV1_COMMON *const cm = &cpi->common;
-  const int is_360p_or_larger = AOMMIN(cm->width, cm->height) >= 360;
   const int is_480p_or_larger = AOMMIN(cm->width, cm->height) >= 480;
   const int is_720p_or_larger = AOMMIN(cm->width, cm->height) >= 720;
   const int is_1080p_or_larger = AOMMIN(cm->width, cm->height) >= 1080;
@@ -280,25 +279,14 @@ static void set_allintra_speed_feature_framesize_dependent(
     if (!is_480p_or_larger) {
       sf->rt_sf.nonrd_check_partition_merge_mode = 2;
     }
-    if (!is_360p_or_larger) {
-      sf->rt_sf.force_tx_search_off = 1;
-    }
   }
 
   if (speed >= 8) {
-    if (!is_360p_or_larger) {
-      sf->rt_sf.use_modeled_non_rd_cost = 0;
-    } else {
-      if (speed == 8) {
-        sf->rt_sf.short_circuit_low_temp_var = 0;
-      }
-    }
+    // TODO(kyslov): add more speed features to control speed/quality
   }
 
   if (speed >= 9) {
-    if (!is_360p_or_larger) {
-      sf->rt_sf.use_modeled_non_rd_cost = 1;
-    }
+    // TODO(kyslov): add more speed features to control speed/quality
   }
 }
 
@@ -522,21 +510,11 @@ static void set_allintra_speed_features_framesize_independent(
   }
 
   if (speed >= 8) {
-    sf->tx_sf.intra_tx_size_search_init_depth_sqr = 2;
-    sf->intra_sf.intra_pruning_with_hog = 1;
-    sf->rt_sf.estimate_motion_for_var_based_partition = 1;
-    sf->rt_sf.short_circuit_low_temp_var = 1;
-    sf->rt_sf.nonrd_check_partition_merge_mode = 0;
-    sf->rt_sf.nonrd_check_partition_split = 0;
-    sf->rt_sf.use_modeled_non_rd_cost = 1;
-    sf->rt_sf.source_metrics_sb_nonrd = 1;
-    sf->rt_sf.skip_intra_pred_if_tx_skip = 0;
+    // TODO(kyslov): add more speed features to control speed/quality
   }
+
   if (speed >= 9) {
-    sf->rt_sf.estimate_motion_for_var_based_partition = 0;
-    sf->rt_sf.force_large_partition_blocks = 1;
-    for (int i = 0; i < BLOCK_SIZES; ++i)
-      sf->rt_sf.intra_y_mode_bsize_mask_nrd[i] = INTRA_DC;
+    // TODO(kyslov): add more speed features to control speed/quality
   }
 
   // Intra txb hash is currently not compatible with multi-winner mode as the
