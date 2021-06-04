1d8912ff648532a02138f9ccc937fefda0141315
Deepa K G <deepa.kg@ittiam.com>
2021-06-04 13:40:14 +0530

    Prune tx types aggressively for winner mode
    The tx type search of winner mode is made faster
    by pruning tx types aggressively for speed 6.
    
    cpu-used  Instruction Count     BD-Rate Loss(%)
               Reduction(%)     avg.psnr  ovr.psnr  ssim
       6          1.81          0.2241    0.2366	0.1508
    
    STATS_CHANGED
    
    Change-Id: Id560635517757a682543c8d5aa08755ca275576f

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1107,7 +1107,7 @@ static void set_good_speed_features_framesize_independent(
     sf->tpl_sf.gop_length_decision_method = 3;
     sf->tpl_sf.disable_filtered_key_tpl = 1;
 
-    sf->tx_sf.tx_type_search.winner_mode_tx_type_pruning = 3;
+    sf->tx_sf.tx_type_search.winner_mode_tx_type_pruning = 4;
     sf->tx_sf.use_intra_txb_hash = 1;
     sf->tx_sf.tx_type_search.prune_tx_type_est_rd = 0;
 
