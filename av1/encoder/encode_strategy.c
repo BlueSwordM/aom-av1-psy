12287adee94fc3b1f5349d3f4bd85cea4e57f62b
Sarah Parker <sarahparker@google.com>
2021-06-08 14:04:42 -0700

    Revert "Fix kf counters in fwd kf"
    This reverts commit 927505150ff5054495e89ea2eb08f5febaedad7a.
    
    BUGS=aomedia:3072
    
    Change-Id: I463ccaaa272a2f5a1ac78068cb7580b63d1e3a3a

--- av1/encoder/encode_strategy.c
+++ av1/encoder/encode_strategy.c
@@ -138,8 +138,7 @@ static void set_additional_frame_flags(const AV1_COMMON *const cm,
 }
 
 static INLINE void update_keyframe_counters(AV1_COMP *cpi) {
-  if (cpi->common.show_frame &&
-      (cpi->rc.frames_to_key || cpi->oxcf.kf_cfg.fwd_kf_enabled)) {
+  if (cpi->common.show_frame && cpi->rc.frames_to_key) {
     cpi->rc.frames_since_key++;
     cpi->rc.frames_to_key--;
   }
