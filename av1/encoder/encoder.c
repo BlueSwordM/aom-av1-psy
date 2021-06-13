6d8fb84cacf84bbf95c3b06b9eb4ea59f3fdb7b4
Jingning Han <jingning@google.com>
2021-06-12 23:39:20 -0700

    Use mean MB variance to determine SB level Wiener variance
    Change-Id: Ic4189a41c6bf03f8dc9223b576745297b3ba1d8e

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -3137,25 +3137,24 @@ static int get_window_wiener_var(AV1_COMP *const cpi, BLOCK_SIZE bsize,
   int sb_wiener_var = 0;
   int mb_stride = cpi->frame_info.mb_cols;
   int mb_count = 0;
-  int mb_wiener_var[64];
+  int64_t mb_wiener_sum = 0;
 
   for (int row = mi_row; row < mi_row + mi_high; row += mi_step) {
     for (int col = mi_col; col < mi_col + mi_wide; col += mi_step) {
       if (row >= cm->mi_params.mi_rows || col >= cm->mi_params.mi_cols)
         continue;
 
-      mb_wiener_var[mb_count] =
+      mb_wiener_sum +=
           (int)cpi->mb_wiener_variance[(row / mi_step) * mb_stride +
                                        (col / mi_step)];
       ++mb_count;
     }
   }
-  qsort(mb_wiener_var, mb_count, sizeof(*mb_wiener_var), qsort_comp);
 
-  sb_wiener_var = mb_wiener_var[mb_count / 3];
+  if (mb_count) sb_wiener_var = (int)(mb_wiener_sum / mb_count);
   sb_wiener_var = AOMMAX(1, sb_wiener_var);
 
-  return sb_wiener_var;
+  return (int)(sqrt(sb_wiener_var));
 }
 
 static int get_var_perceptual_ai(AV1_COMP *const cpi, BLOCK_SIZE bsize,
