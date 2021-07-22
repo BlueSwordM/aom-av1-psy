wjdz99/aom

Add fast version of av1_nn_softmax
This commit adds a faster but less accurate version of av1_nn_softmax
where the output size is 16.

This provides SIMD optimization as well as a linear approximation for
expf. The C code provides 60% encoding time reduction at functional
level, the SSE3 code 89%.

Performance in RT Mode:
SPD_SET | TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   |  SPD
---------+---------+----------+----------+---------+-------
    5    |rtc_derf | -0.156%  | -0.151%  | -0.210% | +0.8%
    5    |   rtc   | -0.050%  | -0.062%  | -0.049% | +0.6%
---------+---------+----------+----------+---------+-------
    6    |rtc_derf | +0.039%  | +0.037%  | +0.037% | +0.7%
    6    |   rtc   | +0.009%  | +0.013%  | -0.015% | +0.8%
---------+---------+----------+----------+---------+-------

Performance in GOOD Mode:
 SPD_SET | TESTSET | AVG_PSNR | OVR_PSNR |  SSIM   |  SPD
---------+---------+----------+----------+---------+-------
    1    | hdres2  | +0.004%  | +0.011%  | +0.004% | +0.2%
    1    | midres2 | +0.016%  | -0.010%  | +0.003% | +0.1%
    1    | lowres2 | -0.018%  | -0.018%  | +0.005% | +0.2%
---------+---------+----------+----------+---------+-------
    2    | hdres2  | -0.080%  | -0.073%  | -0.095% | +0.1%
    2    | midres2 | -0.009%  | -0.010%  | -0.018% | +0.2%
    2    | lowres2 | +0.015%  | +0.013%  | -0.030% | +0.4%
---------+---------+----------+----------+---------+-------
    3    | hdres2  | -0.003%  | +0.003%  | +0.024% | +0.2%
    3    | midres2 | -0.018%  | -0.017%  | -0.047% | +0.2%
    3    | lowres2 | -0.053%  | -0.055%  | -0.050% | +0.4%
---------+---------+----------+----------+---------+-------
    4    | hdres2  | -0.001%  | -0.001%  | -0.001% | -0.0%
    4    | midres2 | +0.001%  | +0.001%  | +0.001% | +0.0%
    4    | lowres2 | +0.000%  | +0.000%  | +0.000% | +0.0%
---------+---------+----------+----------+---------+-------
    5    | hdres2  | +0.000%  | +0.000%  | -0.000% | +0.0%
    5    | midres2 | -0.000%  | -0.000%  | +0.000% 

av1/encoder/ml.c
@@ -149,8 +149,34 @@ void av1_nn_softmax(const float *input, float *output, int n) {
   for (int i = 0; i < n; i++) {
     // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
     const float normalized_input = AOMMAX(input[i] - max_inp, -10.0f);
-    output[i] = (float)exp(normalized_input);
+    output[i] = expf(normalized_input);
     sum_out += output[i];
   }
   for (int i = 0; i < n; i++) output[i] /= sum_out;
+}
+
+AOM_INLINE float approx_exp(float y) {
+#define A ((1 << 23) / 0.69314718056f)  // (1 << 23) / ln(2)
+#define B \
+  127  // Offset for the exponent according to IEEE floating point standard.
+#define C 60801  // Magic number controls the accuracy of approximation
+  const int32_t int_repre = ((int32_t)(y * A)) + ((B << 23) - C);
+  return *(float *)(&int_repre);
+#undef A
+#undef B
+#undef C
+}
+
+void av1_nn_fast_softmax_16_c(const float *input, float *output) {
+  const int num_classes = 16;
+  float max_inp = input[0];
+  for (int i = 1; i < num_classes; i++) max_inp = AOMMAX(max_inp, input[i]);
+  float sum_out = 0.0f;
+  for (int i = 0; i < num_classes; i++) {
+    // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
+    const float normalized_input = AOMMAX(input[i] - max_inp, -10.0f);
+    output[i] = approx_exp(normalized_input);
+    sum_out += output[i];
+  }
+  for (int i = 0; i < num_classes; i++) output[i] /= sum_out;
 }

BlueSwordM/aom-av1-psy

av1/encoder/ml.c
@@ -143,14 +143,44 @@ void av1_nn_softmax(const float *input, float *output, int n) {
   // Softmax function is invariant to adding the same constant
   // to all input values, so we subtract the maximum input to avoid
   // possible overflow.
-  float max_inp = input[0];
-  for (int i = 1; i < n; i++) max_inp = AOMMAX(max_inp, input[i]);
+  float max_input = input[0];
+  for (int i = 1; i < n; i++) max_input = AOMMAX(max_input, input[i]);
   float sum_out = 0.0f;
   for (int i = 0; i < n; i++) {
     // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
-    const float normalized_input = AOMMAX(input[i] - max_inp, -10.0f);
-    output[i] = (float)exp(normalized_input);
+    const float normalized_input = AOMMAX(input[i] - max_input, -10.0f);
+    output[i] = expf(normalized_input);
     sum_out += output[i];
   }
   for (int i = 0; i < n; i++) output[i] /= sum_out;
+}
+
+static AOM_INLINE float approx_exp(float y) {
+#define A ((1 << 23) / 0.69314718056f)  // (1 << 23) / ln(2)
+#define B \
+  127  // Offset for the exponent according to IEEE floating point standard.
+#define C 60801  // Magic number controls the accuracy of approximation
+  union {
+    float as_float;
+    int32_t as_int32;
+  } container;
+  container.as_int32 = ((int32_t)(y * A)) + ((B << 23) - C);
+  return container.as_float;
+#undef A
+#undef B
+#undef C
+}
+
+void av1_nn_fast_softmax_16_c(const float *input, float *output) {
+  const int kNumClasses = 16;
+  float max_input = input[0];
+  for (int i = 1; i < kNumClasses; i++) max_input = AOMMAX(max_input, input[i]);
+  float sum_out = 0.0f;
+  for (int i = 0; i < kNumClasses; i++) {
+    // Clamp to range [-10.0, 0.0] to prevent FE_UNDERFLOW errors.
+    const float normalized_input = AOMMAX(input[i] - max_input, -10.0f);
+    output[i] = approx_exp(normalized_input);
+    sum_out += output[i];
+  }
+  for (int i = 0; i < kNumClasses; i++) output[i] /= sum_out;
 }