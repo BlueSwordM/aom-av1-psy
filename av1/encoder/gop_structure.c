52fa57030199eda879599f5174541a1d3688e1ad
Jingning Han <jingning@google.com>
2021-10-17 20:45:41 -0700

    Set max layer depth for low delay settings
    Properly set the max layer depth for the low delay settings.
    
    Change-Id: I14777394f7a572dab7faee2ee6b5a746c626cf7e

--- av1/encoder/gop_structure.c
+++ av1/encoder/gop_structure.c
@@ -858,6 +858,7 @@ static void set_ld_layer_depth(GF_GROUP *gf_group, int gop_length) {
     }
     gf_group->layer_depth[gf_index] = AOMMAX(log_gop_length - count, 0);
   }
+  gf_group->max_layer_depth = log_gop_length;
 }
 
 void av1_gop_setup_structure(AV1_COMP *cpi) {
