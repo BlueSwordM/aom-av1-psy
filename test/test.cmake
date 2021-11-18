78ce82eb7555b51bc8143d80fec5cb39e4730371
Yunqing Wang <yunqingwang@google.com>
2021-11-18 13:41:25 -0800

    Remove unused edge detection
    Removed unused edge detection and related unit tests. So we
    don't need to maintain and support it.
    
    BUG=aomedia:3188
    
    Change-Id: I92fcee5a34d0949b49ed98ee01176078b68a6418

--- test/test.cmake
+++ test/test.cmake
@@ -257,7 +257,6 @@ if(NOT BUILD_SHARED_LIBS)
               "${AOM_ROOT}/test/comp_avg_pred_test.cc"
               "${AOM_ROOT}/test/comp_avg_pred_test.h"
               "${AOM_ROOT}/test/comp_mask_variance_test.cc"
-              "${AOM_ROOT}/test/edge_detect_test.cc"
               "${AOM_ROOT}/test/encodemb_test.cc"
               "${AOM_ROOT}/test/encodetxb_test.cc"
               "${AOM_ROOT}/test/end_to_end_ssim_test.cc"
