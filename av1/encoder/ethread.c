b2b845b6cbfd6559bd1f451939c01e31442afdb3
Remya Prakasan <remya.prakasan@ittiam.com>
2021-11-22 14:51:36 +0530

    FPMT: Tune num_fp_context calculation for SB 64X64
    Tuned the computation of num_fp_contexts for higher resolutions
    with superblock size 64x64.
    
    Change-Id: I633c21c99b80e72f08aad68709db37576fc77807

--- av1/encoder/ethread.c
+++ av1/encoder/ethread.c
@@ -914,10 +914,23 @@ int av1_compute_num_fp_contexts(AV1_PRIMARY *ppi, AV1EncoderConfig *oxcf) {
   }
   int max_num_enc_workers =
       av1_compute_num_enc_workers(ppi->parallel_cpi[0], MAX_THREADS);
-
-  // A parallel frame encode must have at least 1/4th the theoretical limit of
-  // max enc workers. TODO(Remya) : Tune this value for multi-tile case.
-  int workers_per_frame = AOMMAX(1, (max_num_enc_workers + 2) / 4);
+  // Scaling factors and rounding factors used to tune worker_per_frame
+  // computation.
+  int rounding_factor[2] = { 2, 4 };
+  int scaling_factor[2] = { 4, 8 };
+  int is_480p_or_lesser =
+      AOMMIN(oxcf->frm_dim_cfg.width, oxcf->frm_dim_cfg.height) <= 480;
+  int is_sb_64 = 0;
+  if (ppi->cpi != NULL)
+    is_sb_64 = ppi->cpi->common.seq_params->sb_size == BLOCK_64X64;
+  // A parallel frame encode has at least 1/4th the
+  // theoretical limit of max enc workers in default case. For resolutions
+  // larger than 480p, if SB size is 64x64, optimal performance is obtained with
+  // limit of 1/8.
+  int index = (!is_480p_or_lesser && is_sb_64) ? 1 : 0;
+  int workers_per_frame =
+      AOMMAX(1, (max_num_enc_workers + rounding_factor[index]) /
+                    scaling_factor[index]);
   int max_threads = oxcf->max_threads;
   int num_fp_contexts = max_threads / workers_per_frame;
 
