88879fae5b7e2ebe166f1a322d5a0cc8280d8758
James Zern <jzern@google.com>
2021-12-02 12:25:37 -0800

    WarpedMotionParams: reduce size of wmmat[] to 6
    only indices 0-5 are used, 6-7 were just being zeroed
    
    Change-Id: Ib2eb8f813227b8b94f34e70649d18647914398c5

--- av1/common/warped_motion.c
+++ av1/common/warped_motion.c
@@ -1056,8 +1056,6 @@ static int find_affine_int(int np, const int *pts1, const int *pts2,
       clamp(vx, -WARPEDMODEL_TRANS_CLAMP, WARPEDMODEL_TRANS_CLAMP - 1);
   wm->wmmat[1] =
       clamp(vy, -WARPEDMODEL_TRANS_CLAMP, WARPEDMODEL_TRANS_CLAMP - 1);
-
-  wm->wmmat[6] = wm->wmmat[7] = 0;
   return 0;
 }
 
