2ca8346182a628439345a489eae2fae517c858dd
Marco Paniconi <marpan@google.com>
2021-12-16 10:19:44 -0800

    rtc: Fix to partition threshold increase for low-resln
    For sf->force_large_partition_blocks = 1 (speed >= 9)
    and low resoultions: fix the partition threshold increase
    by conditioning it on the segment and source sad.
    This reduces regression on rtc_derf for several clips.
    
    bdrate gain on rtc_derf: ~2%, several clips 3-4%.
    speed slowdown ~3%.
    
    Change-Id: I79054e3fd16e0fb229581a21243e623c82f8294d

--- av1/encoder/var_based_part.c
+++ av1/encoder/var_based_part.c
@@ -461,9 +461,14 @@ static AOM_INLINE void set_vbp_thresholds(AV1_COMP *cpi, int64_t thresholds[],
         }
       }
       if (cm->width * cm->height <= 352 * 288) {
-        thresholds[1] <<= 2;
-        thresholds[2] <<= 5;
         thresholds[3] = INT32_MAX;
+        if (segment_id == 0) {
+          thresholds[1] <<= 2;
+          thresholds[2] <<= (source_sad == kLowSad) ? 5 : 4;
+        } else {
+          thresholds[1] <<= 1;
+          thresholds[2] <<= 3;
+        }
         // Condition the increase of partition thresholds on the segment
         // and the content. Avoid the increase for superblocks which have
         // high source sad, unless the whole frame has very high motion
