de1db21bbc845542aa42c6b4500087d0374cc962
Angie Chiang <angiebird@google.com>
2022-03-15 22:17:16 -0700

    Add utility functions to test GopStruct
    Bug: b/221916304
    
    Change-Id: Idbe060f6ed2a7feec01504ab7031f6171a65b337

--- test/ratectrl_qmode_test.cc
+++ test/ratectrl_qmode_test.cc
@@ -14,34 +14,31 @@
 #include "av1/ratectrl_qmode.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 namespace aom {
-TEST(RateControlQModeTest, ConstructGop) {
-  int show_frame_count = 16;
-  int max_ref_frames = 7;
-  bool has_key_frame = false;
-  RefFrameManager ref_frame_manager(max_ref_frames);
-  GopStruct gop_struct =
-      construct_gop(&ref_frame_manager, show_frame_count, has_key_frame);
-  int ref_order_idx = 0;
+
+void test_gop_display_order(const GopStruct &gop_struct) {
   // Test whether show frames' order indices are sequential
+  int ref_order_idx = 0;
   for (const auto &gop_frame : gop_struct.gop_frame_list) {
     if (gop_frame.is_show_frame) {
       EXPECT_EQ(gop_frame.order_idx, ref_order_idx);
       ref_order_idx++;
     }
   }
+}
 
+void test_colocated_show_frame(const GopStruct &gop_struct) {
   // Test whether each non show frame has a colocated show frame
-  int gop_size = static_cast<int>(gop_struct.gop_frame_list.size());
-  for (int gop_idx = 0; gop_idx < gop_size; ++gop_idx) {
+  size_t gop_size = gop_struct.gop_frame_list.size();
+  for (size_t gop_idx = 0; gop_idx < gop_size; ++gop_idx) {
     auto &gop_frame = gop_struct.gop_frame_list[gop_idx];
     if (gop_frame.is_show_frame == 0) {
       bool found_colocated_ref_frame = false;
-      for (int i = gop_idx + 1; i < gop_size; ++i) {
+      for (size_t i = gop_idx + 1; i < gop_size; ++i) {
         auto &next_gop_frame = gop_struct.gop_frame_list[i];
         if (gop_frame.order_idx == next_gop_frame.order_idx) {
           found_colocated_ref_frame = true;
           EXPECT_EQ(gop_frame.update_ref_idx, next_gop_frame.colocated_ref_idx);
-          EXPECT_EQ(next_gop_frame.is_show_frame, 1);
+          EXPECT_TRUE(next_gop_frame.is_show_frame);
         }
         if (gop_frame.update_ref_idx == next_gop_frame.update_ref_idx) {
           break;
@@ -50,6 +47,17 @@ TEST(RateControlQModeTest, ConstructGop) {
       EXPECT_TRUE(found_colocated_ref_frame);
     }
   }
+}
+
+TEST(RateControlQModeTest, ConstructGopARF) {
+  int show_frame_count = 16;
+  const int max_ref_frames = 7;
+  const bool has_key_frame = false;
+  RefFrameManager ref_frame_manager(max_ref_frames);
+  GopStruct gop_struct =
+      construct_gop(&ref_frame_manager, show_frame_count, has_key_frame);
+  test_gop_display_order(gop_struct);
+  test_colocated_show_frame(gop_struct);
 }
 
 }  // namespace aom
