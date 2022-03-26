c3cbaa2f9e018510c0bf1275cf50dd5f7932c546
BlueSwordM <zakdjeb@gmail.com>
2022-03-26 15:38:36 -0400

    Adding perceptual image tune

--- av1/encoder/partition_search.c
+++ av1/encoder/partition_search.c
@@ -646,7 +646,8 @@ static void setup_block_rdmult(const AV1_COMP *const cpi, MACROBLOCK *const x,
   }
 #endif  // !CONFIG_REALTIME_ONLY
 
-  if (cpi->oxcf.tune_cfg.tuning == AOM_TUNE_SSIM) {
+  if (cpi->oxcf.tune_cfg.tuning == AOM_TUNE_SSIM ||
+      cpi->oxcf.tune_cfg.tuning == AOM_TUNE_IMAGE_PERCEPTUAL_QUALITY) {
     av1_set_ssim_rdmult(cpi, &x->errorperbit, bsize, mi_row, mi_col,
                         &x->rdmult);
   }
