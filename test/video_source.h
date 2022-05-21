2226b716878121bb1e3e1ddff8cf360c51f5c5de
James Zern <jzern@google.com>
2022-05-20 19:15:47 -0700

    GetTempOutFile(): use testing::TempDir()
    rather than '/tmp/'. this allows for setting the path with TEST_TMPDIR
    and provides a valid default for android.
    
    Change-Id: Iecb26f381b6a6ec97da62cfa0b7200f427440a2f

--- test/video_source.h
+++ test/video_source.h
@@ -19,6 +19,8 @@
 #endif
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+#include <memory>
 #include <string>
 #include "test/acm_random.h"
 #include "aom/aom_encoder.h"
@@ -71,9 +73,22 @@ static FILE *GetTempOutFile(std::string *file_name) {
   }
   return NULL;
 #else
-  char name_template[] = "/tmp/libaomtest.XXXXXX";
-  const int fd = mkstemp(name_template);
-  *file_name = name_template;
+  std::string temp_dir = testing::TempDir();
+  if (temp_dir.empty()) return NULL;
+  // Versions of testing::TempDir() prior to release-1.11.0-214-g5e6a5336 may
+  // use the value of an environment variable without checking for a trailing
+  // path delimiter.
+  if (temp_dir[temp_dir.size() - 1] != '/') temp_dir += '/';
+  const char name_template[] = "libaomtest.XXXXXX";
+  std::unique_ptr<char[]> temp_file_name(
+      new char[temp_dir.size() + sizeof(name_template)]);
+  if (temp_file_name == nullptr) return NULL;
+  memcpy(temp_file_name.get(), temp_dir.data(), temp_dir.size());
+  memcpy(temp_file_name.get() + temp_dir.size(), name_template,
+         sizeof(name_template));
+  const int fd = mkstemp(temp_file_name.get());
+  if (fd == -1) return NULL;
+  *file_name = temp_file_name.get();
   return fdopen(fd, "wb+");
 #endif
 }
