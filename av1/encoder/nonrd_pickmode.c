55b68dbf23803f70ae802518362364481cec305c
Marco Paniconi <marpan@google.com>
2022-06-07 12:51:06 -0700

    rtc-screen: Adjust skip of newmv for flat blocks.
    Remove the conditon of slide change for skipping
    newmv for blocks with low spatial variance.
    
    Neutral/small bdrate loss on speed 10 (~0.2%),
    but several clips ~2% instruction_count speedup.
    
    Change-Id: I3b386bcf88641b74faf5e10d8371763ea627460c

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -2834,10 +2834,8 @@ void av1_nonrd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
              x->source_variance == 0))
           continue;
       }
-      // Skip NEWMV search on scene cuts for flat blocks.
-      if (cpi->rc.high_source_sad && this_mode == NEWMV &&
-          (x->source_variance < 100))
-        continue;
+      // Skip NEWMV search for flat blocks.
+      if (this_mode == NEWMV && x->source_variance < 100) continue;
     }
 
     if (skip_mode_by_bsize_and_ref_frame(
