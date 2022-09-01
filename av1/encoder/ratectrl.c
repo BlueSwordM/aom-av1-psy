0e6679bb5a5cc71cb1bed6f7d2797a2db52d900a
Paul Wilkins <paulwilkins@google.com>
2022-09-01 14:45:06 +0100

    Constrain Q adjustment.
    This patch modifies the constraints on Q adjustment between
    frames for RTC.
    
    Previously the maximum downward Q adjustment was 8 for
    screen content and 16 for other content, This change makes
    the maximum downward adjustment depend on the value of
    Q for the last frame.
    
    The change helps reduce large data spikes that can occur
    when the frame to frame Q drops too fast, especially at
    low Q.
    
    Adjustments made to original patch to improve metrics
    on non screen content. Also these reduced the impact
    for screen content.
    
    %BDRATE opsnr, ssim (-ve is better).
    
    Speed 8
    derf	-0.054	-0.028
    rtc	-0.024	0.008
    1080p	0.075	0.062
    Screen	-3.487	-2.642
    
    Speed 10
    derf	0.024	0.096
    rtc	0.112	-0.121
    1080p	-0.128	-0.211
    Screen	-1.804	-1.317
    
    STATS_CHANGED
    
    Change-Id: I9d0679df12749b55eb92fccd9c65cc3ce128b289

--- av1/encoder/ratectrl.c
+++ av1/encoder/ratectrl.c
@@ -447,8 +447,9 @@ static int adjust_q_cbr(const AV1_COMP *cpi, int q, int active_worst_quality) {
   const PRIMARY_RATE_CONTROL *const p_rc = &cpi->ppi->p_rc;
   const AV1_COMMON *const cm = &cpi->common;
   const RefreshFrameInfo *const refresh_frame = &cpi->refresh_frame;
-  const int max_delta_down =
-      (cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN) ? 8 : 16;
+  const int max_delta_down = (cpi->oxcf.tune_cfg.content == AOM_CONTENT_SCREEN)
+                                 ? AOMMIN(8, AOMMAX(1, rc->q_1_frame / 16))
+                                 : AOMMIN(16, AOMMAX(1, rc->q_1_frame / 8));
   const int max_delta_up = 20;
   const int change_avg_frame_bandwidth =
       abs(rc->avg_frame_bandwidth - rc->prev_avg_frame_bandwidth) >
