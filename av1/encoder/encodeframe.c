97459488f0d72373e97e9595ec2f7ce23f5fb7f6
BlueSwordM <notgettingmyownemaillmao@gmail.com>
2022-09-13 17:55:37 -0400

    All-frames SB RD adjustment for psy test(adding inter one later)

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -914,7 +914,7 @@ static AOM_INLINE void encode_sb_row(AV1_COMP *cpi, ThreadData *td,
     x->content_state_sb.low_sumdiff = 0;
     x->force_zeromv_skip_for_sb = 0;
 
-    if (cpi->oxcf.mode == ALLINTRA) {
+    if (cpi->oxcf.mode == ALLINTRA || cpi->oxcf.tune_cfg.content == AOM_CONTENT_PSY) {
       x->intra_sb_rdmult_modifier = 128;
     }
 
