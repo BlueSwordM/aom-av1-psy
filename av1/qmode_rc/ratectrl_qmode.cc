ed45d929c03fa7e14c5564df1cb649791685aaa7
Jingning Han <jingning@google.com>
2022-09-28 10:18:01 -0700

    Use a structured frame qp offset decision in qmode
    This allows the rate control decision process to handle the static
    videos with a more stable approach.
    
    BUG=b/249276612
    
    Change-Id: I407a5a5ee1ff5eba2d9767df7e206844d9d7134d

--- av1/qmode_rc/ratectrl_qmode.cc
+++ av1/qmode_rc/ratectrl_qmode.cc
@@ -1288,14 +1288,19 @@ StatusOr<GopEncodeInfo> AV1RateControlQMode::GetGopEncodeInfo(
   }
   const int frame_count =
       static_cast<int>(tpl_gop_stats.frame_stats_list.size());
+  const int active_worst_quality = rc_param_.base_q_index;
+  int active_best_quality = rc_param_.base_q_index;
   for (int i = 0; i < frame_count; i++) {
     FrameEncodeParameters param;
     const GopFrame &gop_frame = gop_struct.gop_frame_list[i];
 
     if (gop_frame.update_type == GopFrameType::kOverlay ||
-        gop_frame.update_type == GopFrameType::kIntermediateOverlay) {
+        gop_frame.update_type == GopFrameType::kIntermediateOverlay ||
+        gop_frame.update_type == GopFrameType::kRegularLeaf) {
       param.q_index = rc_param_.base_q_index;
-    } else {
+    } else if (gop_frame.update_type == GopFrameType::kRegularGolden ||
+               gop_frame.update_type == GopFrameType::kRegularKey ||
+               gop_frame.update_type == GopFrameType::kRegularArf) {
       const TplFrameDepStats &frame_dep_stats =
           gop_dep_stats->frame_dep_stats_list[i];
       const double cost_without_propagation =
@@ -1309,6 +1314,14 @@ StatusOr<GopEncodeInfo> AV1RateControlQMode::GetGopEncodeInfo(
       param.q_index = av1_get_q_index_from_qstep_ratio(rc_param_.base_q_index,
                                                        qstep_ratio, AOM_BITS_8);
       if (rc_param_.base_q_index) param.q_index = AOMMAX(param.q_index, 1);
+      active_best_quality = param.q_index;
+    } else {
+      // Intermediate ARFs
+      assert(gop_frame.layer_depth >= 1);
+      const int depth_factor = 1 << (gop_frame.layer_depth - 1);
+      param.q_index =
+          (active_worst_quality * (depth_factor - 1) + active_best_quality) /
+          depth_factor;
     }
     param.rdmult = GetRDMult(gop_frame, param.q_index);
     gop_encode_info.param_list.push_back(param);
