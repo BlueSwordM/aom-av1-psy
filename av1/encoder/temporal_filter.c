8bfe93c43c3c3d6a7567cca90107d9b6775d1c51
Bohan Li <bohanli@google.com>
2022-11-21 09:41:07 -0800

    Fix buf overflow when doing temporal filtering for fwd-kf.
    Change-Id: Ib9c07d3e889d012c3c1d34cba3e3466d27ae1083

--- av1/encoder/temporal_filter.c
+++ av1/encoder/temporal_filter.c
@@ -1040,7 +1040,7 @@ static void tf_setup_filtering_buffer(AV1_COMP *cpi,
   num_frames = AOMMIN(num_frames + adjust_num, lookahead_depth);
 
   if (frame_type == KEY_FRAME) {
-    num_before = is_forward_keyframe ? num_frames / 2 : 0;
+    num_before = AOMMIN(is_forward_keyframe ? num_frames / 2 : 0, max_before);
     num_after = AOMMIN(num_frames - 1, max_after);
   } else {
     int gfu_boost = av1_calc_arf_boost(&cpi->ppi->twopass, &cpi->twopass_frame,
